5eb0c8dcb9ee9d1295067daec08994eaa0268d75
chasegibs <133797904+chasegibs@users.noreply.github.com>
2025-12-04 19:44:27 -0600

    fixed nitro
    work done with caleb to fix some of the issues i was having, code is now in a state that only errors are sql errors.

--- server/plugins/nitroPlugin.ts
+++ server/plugins/nitroPlugin.ts
@@ -1,76 +1,42 @@
-import 'dotenv/config';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '../../app/generated/prisma';
+import {prisma} from '../utils/prisma';
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL ?? '',
-});
-
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
-
-export default defineNitroPlugin((nitroApp) => {
+export default defineNitroPlugin( async (nitroApp) => {
 
   console.log('Nitro plugin', nitroApp)
 
-async function main() {
-  
     const user = await prisma.user.create({
         data: {
-        user_name:  'adrian',
-        user_email: 'fake@email.com',
-        user_phone: '123-456-7890',
-        attendance_history: { 
-            create: {
-            clock_in_time: ("2025-01-15T12:00:00Z"),
-            clock_out_time: ("2025-01-15T17:00:00Z"),
-            status: 'PRESENT',
-            }
-        },
-
-        schedule: {
-             create:{
-                date: new Date("2025-01-15T17:00:00Z"),
-                shift_start: 1200,
-                shift_end: 1500,
-                status: "on_site",
-
+            user_name:  'adrian',
+            user_email: 'fake@email.com',
+            user_phone: '123-456-7890',
+            attendance_history: { 
+                create: {
+                clock_in_time: ("2025-01-15T12:00:00Z"),
+                clock_out_time: ("2025-01-15T17:00:00Z"),
+                status: 'PRESENT',
                 }
-        },
+            },
 
-        location:{
-            create:{
-                location_ID: 1,
-                location_name: 'place',
-                address: 'adress',
-                operation_days: 'MONDAY',
-                open_time: 1200,
-                close_time: 1500,
-                attendance_code: 1555,
-                userId: 1
-            }
+            schedule: {
+                create:{
+                    date: new Date("2025-01-15T17:00:00Z"),
+                    shift_start: 1200,
+                    shift_end: 1500,
+                    status: "on_site",
+                    }
+            },
+
+            location:{
+                create:{
+                    location_name: 'place',
+                    address: 'adress',
+                    operation_days: 'MONDAY',
+                    open_time: 1200,
+                    close_time: 1500,
+                    attendance_code: 1555,
+                }
+            },
         },
-   
-    },
-    include: {},
     })
-  console.log('created user:', user)
-
-   const allUsers = await prisma.user.findMany({
-    include: {},
-  })
-  console.log('All users:', JSON.stringify(allUsers, null, 2))
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
- 
+    console.log('created user:', user)
 })
