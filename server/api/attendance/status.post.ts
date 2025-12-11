321ee8be4d275a68825a6905b25a456dc9848c4f
chasegibs <133797904+chasegibs@users.noreply.github.com>
2025-12-11 14:51:22 -0600

    small changes

---
+++ server/api/attendance/status.post.ts
@@ -0,0 +1,27 @@
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
+
+  const {
+    id,           
+    userID,         
+    clock_in_time,
+    clock_out_time,
+    status,
+    request_status,
+    reason
+  } = body
+
+   const updated = await prisma.attendance.update({
+    where: { id },
+    data: {
+        clock_in_time:  body.clock_in_time,
+        clock_out_time: body.clock_out_time,
+        status:         body.status,
+        request_status: body.request_status,
+        reason:         body.reason,
+    }
+  })
+
+  return updated
+
+})
