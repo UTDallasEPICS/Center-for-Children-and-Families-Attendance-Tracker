4a52073698662ed21b4989ad80945afbb46c4041
beesoncaleb <caleb.f.beeson@gmail.com>
2026-01-20 23:27:48 -0600

    Fixed schema to be up to date with prisma 7

---
+++ prisma/migrations/20260121052552_spring26/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Location_address_key";
+
+-- DropIndex
+DROP INDEX "Location_location_name_key";
+
+-- DropIndex
+DROP INDEX "User_user_phone_key";
+
+-- DropIndex
+DROP INDEX "User_user_email_key";
