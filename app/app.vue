a880819525c66f76b2c6e5734792b67e436a873f
nairpie <jasteway@gmail.com>
2026-03-23 16:33:53 -0500

    navbar prototype

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="min-h-screen bg-[#f4f4f4]">
     <!-- FULL WIDTH HEADER -->
-    <header class="fixed top-0 left-0 w-full h-[calc(55px+env(safe-area-inset-top))] pt-[env(safe-area-inset-top)] bg-[#00593f] flex justify-end items-center pr-[30px] z-[999]">
-      <HamburgerMenu />
+    <header class="fixed w-full z-20 top-0 start-0 bg-[#E87500]">
+      <nav class="bg-neutral-primary">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-x1 p-4">
+          <!-- put the link here i forgor-->
+          <a href="link" class="flex items-center space-x-3 rtl:space-x-reverse"> 
+            <!-- images for navbar like utd logo-->
+          </a>
+        </div>
+      </nav>
+      <nav class="bg-neutral-secondary-soft border-y border-default border-default">
+        <div class="max-w-screen-xl px-4 py-3 mx-auto">
+          <div class="flex items-center">
+            <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <!-- where to add links and sections on navbar-->
+              <li>
+                <a href="#" class="text-heading hover:underline" aria-current="page">Check In</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
 
     <!-- Page Content -->
     <NuxtPage />
   </div>
 </template>
+
+
