9f8891e90f32f5f4df4883116b53e0fac166ff43
NishantKK2004 <bt7274@DESKTOP-3V63ETP.localdomain>
2026-03-30 16:37:28 -0500

    Created final site manager API to update attendance records for users. Removed date validation from code/[site_id].get.ts. All API's pass test cases looking to create a new seed file for API inputs to unify desired outputs.

--- server/api/site/code/[site_id].get.ts
+++ server/api/site/code/[site_id].get.ts
@@ -1,7 +1,6 @@
 export default defineEventHandler(async (event) => {
   const site_id = event.context.params?.site_id as string
 
-  // Error if no site id is given
   if (!site_id) {
     throw createError({
       statusCode: 400,
@@ -9,27 +8,15 @@ export default defineEventHandler(async (event) => {
     })
   }
 
-  // Date validation
-  const now = new Date()
-
-  const startOfDay = new Date(now)
-  startOfDay.setHours(0, 0, 0, 0)
-
-  const endOfDay = new Date(now)
-  endOfDay.setHours(23, 59, 59, 999)
-
-  // Make sure attendance code is generated at site and during the same day
   const site = await prisma.location.findUnique({
     where: {
       location_ID: site_id
     },
     select: {
-      attendance_code: true,
-      code_generated_at: true 
+      attendance_code: true
     }
   })
 
-  // No code generated for this site
   if (!site || !site.attendance_code) {
     throw createError({
       statusCode: 404,
@@ -37,19 +24,6 @@ export default defineEventHandler(async (event) => {
     })
   }
 
-  // No code generated for this day
-  if (site.code_generated_at) {
-    const generated = new Date(site.code_generated_at)
-
-    if (generated < startOfDay || generated > endOfDay) {
-      throw createError({
-        statusCode: 404,
-        statusMessage: "Attendance code has not been generated for today"
-      })
-    }
-  }
-
-  
   setHeader(event, "Content-Type", "text/plain")
 
   return site.attendance_code
