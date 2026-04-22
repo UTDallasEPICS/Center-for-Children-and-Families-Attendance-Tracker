27a29d97a3a905ee53450f5f01e2bd828fa437de
chasegibs <chase@Chaseslaptop.localdomain>
2026-04-22 15:44:00 -0500

    updated intern apis

--- server/api/intern/index.post.ts
+++ server/api/intern/index.post.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
     const query = getQuery(event)
 
-    const user = prisma.user.create({
+    const user = prisma.intern.create({
         data: {
             user_name: query.user_name as string,
             user_email: query.user_email as string,
@@ -11,6 +11,6 @@ export default defineEventHandler(async (event) => {
         },
     })
 
-    console.log("user created")
+    console.log("intern created")
     return user
 })
