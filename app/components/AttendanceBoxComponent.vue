297be8813654205ecebb79e891fc2279430564c8
chasegibs <chase@Chaseslaptop.localdomain>
2026-04-29 18:03:47 -0500

    site manager box prop made

---
+++ app/components/AttendanceBoxComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  textColor: {
+    type: String,
+    default: 'var(--color-success)'
+  },
+  bgColor: {
+    type: String,
+    default: 'var(--color-success-bg)'
+  },
+  count:{
+    type: Number,
+    default: 0
+  },
+  attType:{
+    type: String,
+    default: 'present'
+  }
+})
+</script>
+
+<template>
+  <div
+    class="w-[78px] h-[52px] pt-1 rounded-md flex flex-col items-center text-sm"
+    :style="{ backgroundColor: bgColor, color: textColor }"
+  >
+    {{ count }}
+    <div>{{ attType }}</div>
+  </div>
+</template>
