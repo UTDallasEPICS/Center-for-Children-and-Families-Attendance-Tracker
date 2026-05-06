b7b537ca898ce9c3d902fd33cc3740b7fc9ce256
nairpie <jasteway@gmail.com>
2026-05-05 22:42:50 -0500

    dropdown made using USelectMenu

---
+++ app/components/Dropdown.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { SelectMenuItem } from '@nuxt/ui'
+
+const props = defineProps<{
+  current: SelectMenuItem
+  items: SelectMenuItem[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:current', value: SelectMenuItem): void
+}>()
+
+const selected = computed({
+  get: () => props.current,
+  set: (val) => emit('update:current', val)
+})
+</script>
+
+<template>
+  <USelectMenu
+    v-model="selected"
+    :items="items"
+    value-key="id"
+    placeholder="Select"
+    class="w-48"
+  />
+</template>
